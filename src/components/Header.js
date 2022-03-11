af22c5351599936dbc341e19eeed9e12f669273a
chris <mr.chrisnash@gmail.com>
2022-03-11 14:46:20 -0800

    menu toggle and handle resize

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import { BiMenuAltRight } from 'react-icons/bi'
-import { AiOutlineClose } from 'react-icons/bi'
+import { AiOutlineClose } from 'react-icons/ai'
 
 import classes from './Header.module.scss'
 
 const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [size, setSize] = useState({
+    width: undefined,
+    height: undefined,
+  });
+
+  useEffect(() => {
+    const handleResize = () => {
+      setSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    }
+    window.addEventListener('resize', handleResize);
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  useEffect(() => {
+    if (size.width > 768 && menuOpen) {
+      setMenuOpen(false);
+    }
+  }, [size.width, menuOpen])
+
+  const menuToggleHandler = () => {
+    setMenuOpen((p) => !p);
+  }
+
   return (
     <header className={classes.header} >
       <div className={classes.header__content}>
         <h2 className={classes.header__content__logo} >navbar</h2>
 
-        <nav className={classes.header__content__nav}>
+        <nav className={`${classes.header__content__nav} ${menuOpen ? classes.isMenu : ""}`}>
           <ul>
             <li>
               <a href='/'>Page One</a>
@@ -27,7 +55,7 @@ const Header = () => {
         </nav>
 
         <div className={classes.header__content__toggle}>
-          <BiMenuAltRight />
+          {!menuOpen ? <BiMenuAltRight onClick={menuToggleHandler} /> : <AiOutlineClose onClick={menuToggleHandler} /> }
         </div>
 
       </div>
